a24751d8d24cba64fcae8d8b916edbe4bbb820a9
Nilloss <43093721+Nilloss@users.noreply.github.com>
2021-12-16 08:28:57 +1000

    Add files via upload

---
+++ Tool Rental Web application/ViewModels/CustomerRentalDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToolRental.Models;
+
+namespace ToolRental.ViewModels
+{
+    public class CustomerRentalDetailsViewModel
+    {
+        public int RentalId { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime DateRented { get; set; }
+        public List<RentedToolViewModel> RentalTools { get; set; }
+    }
+}
---
+++ Tool Rental Web application/ViewModels/RentedToolViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToolRental.ViewModels
+{
+    public class RentedToolViewModel
+    {
+        public int ToolId { get; set; }
+        public string ToolName { get; set; }
+    }
+}
